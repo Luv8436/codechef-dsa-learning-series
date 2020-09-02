6f2acc9b520c65391b4f7141ab0b46223cf62a53
Luvkumar8436 <luvkumar8436@gmail.com>
2020-09-02 21:10:56 +0530

    trying this problem once more

--- Linear-data-structure/ZCO12001.cpp
+++ Linear-data-structure/ZCO12001.cpp
@@ -7,12 +7,15 @@ int main()
 {
 	ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-	int n;
+	ll n;
+	cin >> n;
 	stack <int> s;
-	int depth = 0;
-	int max_depth = 0;
-	int index =0;
-	int max_element = 0;
+	ll depth = 0;
+	ll max_depth = 0;
+	ll index =1;
+	ll max_element = 0;
+	ll curr_element = 0;
+	ll max_element_index = 1;
 	for(int i=0;i<n;i++)
 	{
 		int x;
@@ -20,22 +23,30 @@ int main()
 		if(x==1)
 		{
 			s.push(1);
+			depth += 1;
 		}
 		else
 		{
 			s.pop();
-			depth += 2;
+			curr_element += 2;
 			if(s.empty())
 			{
-				if(depth > max_depth)
+				if(curr_element > max_element )
 				{
-					max_depth = depth;
-					index = i - depth + 1;
+					max_element = curr_element ;
+					max_element_index = i - max_element + 2;
 				}
-				depth = 0;
+				curr_element = 0;
 			}
+			if(depth > max_depth)
+			{
+				max_depth = depth;
+				index = i ;
+			}
+			depth = 0;
 		}
 		
 	}
+	cout << max_depth << " " << index << " " << max_element << " "<< max_element_index << endl;
 
 }
